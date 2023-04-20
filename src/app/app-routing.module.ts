b375d10bb305370e82236978c14734a9405252a4
Alejandro Depetris <aledepetris@outlook.com>
2023-04-20 19:59:42 -0300

    Creación de todos los modulos y componentes que seran utilizados para la construccion de la aplicación. Generación de las rutas padres e hijas, usando lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from '@angular/core';
+import { LayoutPageComponent } from "./auth/pages/layout-page/layout-page.component";
+import { Error404PageComponent } from "./shared/pages/error404-page/error404-page.component";
 
 
 const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule )
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule )
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
 
 ];
 
