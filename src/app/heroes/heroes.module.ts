762e7238bb404f1193889f24ea155196e253deea
Alejandro Depetris <aledepetris@outlook.com>
2023-04-23 15:31:27 -0300

    Mejoras en el componente de las tarjetas, importaicon del modulo de forms reacitvos para su uso, creación de autocompletado en barra de busqueda.

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { MaterialModule } from '../material/material.module';
 
+import { CardComponent } from './components/card/card.component';
+import { HeroImagePipe } from './pipes/hero-image.pipe';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
-import { CardComponent } from './components/card/card.component';
-import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 
 @NgModule({
@@ -27,6 +28,7 @@ import { HeroImagePipe } from './pipes/hero-image.pipe';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     HeroesRoutingModule,
     MaterialModule
   ]
