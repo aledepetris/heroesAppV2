1bc20540fe3ca5be9263e43f3560088040815a66
Alejandro Depetris <aledepetris@outlook.com>
2023-04-27 19:53:23 -0300

    - Generación del metodo checkAutentication. Ejemplo de chequeo de autenticacion en app.component - NO debe realizarse de esta manera (Ver comentarios)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'heroesApp';
+export class AppComponent { //implements OnInit {
+    title = 'heroesApp';
+
+    // Este metodo carga toda la aplicación y luego autentica y redirecciona,
+    // esto genera un problema de seguridad.
+
+    //  constructor( private authService: AuthService ) {};
+
+    // ngOnInit(): void {
+    //     this.authService.checkAuthentication()
+    //         .subscribe( () => {
+    //             console.log("checkAuth finished")
+    //         })
+    // }
+
 }
